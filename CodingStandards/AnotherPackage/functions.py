3f98c9c8fc3e0d520c2274e1c6073a4636817751
nicomp42 <nicholdw@ucmail.uc.edu>
2023-09-19 07:35:46 -0400

    Initial Commit

---
+++ CodingStandards/AnotherPackage/functions.py
@@ -0,0 +1,16 @@
+# functions.py
+# IS4010 required documentation here
+
+if __name__ == "__main__":
+    # We should never get here because this is never an entry point
+    raise Exception("Invalid use of this module. It is not an entry point")
+
+# Put our functions and classes here
+def someFunction(paramater1, paramater2):
+    '''
+    This function ...
+    @param: paramater1 description
+    @param: paramater2 description
+    @return: what the function sends back in a return Statement
+    '''
+    pass
---
+++ CodingStandards/mainPackage/main.py
@@ -0,0 +1,11 @@
+# main.py
+# IS4010 required documentation here
+
+from AnotherPackage.functions import *
+
+if __name__ == "__main__":
+    # All the code goes here
+    #pass   # Remove this when you add some code
+    returnedValue = someFunction("abc", 123)
+    
+    
